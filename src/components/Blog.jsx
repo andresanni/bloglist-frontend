57c01835e367ba1c5b2cf5e5d7d15dea5c3179ea
andresanni <andres.anni1985@gmail.com>
2024-05-13 10:15:04 -0300

    Agregamos la funcionalidad de desplegar los detalles de la lista de blogposts

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,49 @@
-const Blog = ({ blog }) => (
+import { useState } from "react";
+
+const Blog = ({blog})=>{
+
+const [fullView, setFullView] = useState(false);
+
+const handleFullView = ()=>{
+  setFullView(!fullView);
+}
+
+const blogFullViewStyle = {
+  display: "flex",
+  flexDirection:"column",
+  border: "1px solid black"
+}
+
+return(
+
   <div>
-    {blog.title} {blog.author.username}
+    {
+      fullView ?
+      (
+      <div style={blogFullViewStyle}>
+        <div>
+        {blog.title} 
+        <button onClick = {handleFullView}>hide</button> 
+        </div>
+        <p>{blog.url}</p>
+        <div>
+        likes:{blog.likes} <button>like</button>
+        </div>
+        {blog.author.username}      
+      </div>
+      )
+      :
+      (
+      <div>
+        {blog.title} 
+        <button onClick={handleFullView}>view</button>
+      </div>
+      )
+
+    }
   </div>  
 )
+};
+
 
 export default Blog
